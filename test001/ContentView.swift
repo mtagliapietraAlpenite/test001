c3a6f76c498b2498c4656197d9e05baa6be51d23
Matteo Tagliapietra <matteo.tagliapietra.dev@gmail.com>
2024-02-27 16:31:51 +0100

    Initial Commit

---
+++ test001/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test001
+//
+//  Created by T4GLIA PRODUCTIONS on 27/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test001/test001App.swift
@@ -0,0 +1,17 @@
+//
+//  test001App.swift
+//  test001
+//
+//  Created by T4GLIA PRODUCTIONS on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct test001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
