a531933c44ea2a4faea0e24cf1691ac787c35487
Matteo Tagliapietra <matteo.tagliapietra.dev@gmail.com>
2024-02-28 14:59:25 +0100

    changed deprecated component

--- test001/ContentView.swift
+++ test001/ContentView.swift
@@ -9,23 +9,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         // view dotata di barra laterale collassabile
-        NavigationView {
+        NavigationStack {
+            List {
+                
+            }
             // aggiungo un titolo alla navigazione
             Text("Hello, world!")
                 .padding()
         }
-//        // set the view in 2 columns
-//        HStack {
-//            // add a cloasble navigation view
-//            // make the navigation view closable by a button
-//            NavigationView {
-//                // add a title to the navigation view
-//                Text("Hello, world!")
-//                    .padding()
-//            }
-//            Text("Hello, world!")
-//                .padding()
-//        }
     }
 }
 
